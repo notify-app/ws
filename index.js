58fa90b967c934dffa33416893888eab77ac4150
Luca Tabone <taboneluca3@gmail.com>
2016-12-12 14:57:42 +0100

    Change 'name' field in session config to 'cookie'

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function processArgs () {
   const argv = require('minimist')(process.argv.slice(2))
 
   if ('dbURL' in argv) config.db.url = argv.dbURL
-  if ('sessionName' in argv) config.session.name = argv.sessionName
+  if ('sessionCookie' in argv) config.session.cookie = argv.sessionCookie
   if ('sessionMaxAge' in argv) config.session.maxAge = argv.sessionMaxAge
   if ('port' in argv) config.port = argv.port
   if ('instances' in argv) config.instances = argv.instances
